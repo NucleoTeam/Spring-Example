a5080574284eb0159247452828d8d376179a5915
Nathaniel <nathaniel.davidson@gmail.com>
2019-11-24 22:01:04 -0800

    updated nucleo version

--- src/main/java/com/nucleomesh/spring/springexample/rest/RootPage.java
+++ src/main/java/com/nucleomesh/spring/springexample/rest/RootPage.java
@@ -20,103 +20,114 @@ import java.util.TreeMap;
 
 @RestController
 public class RootPage {
-  @Autowired
-  NucleoMeshService meshService;
+    @Autowired
+    NucleoMeshService meshService;
 
-  Logger logger = LoggerFactory.getLogger(RootPage.class);
+    Logger logger = LoggerFactory.getLogger(RootPage.class);
 
-  @GetMapping("/prev")
-  public DeferredResult<ResponseEntity<?>> getPrev(){
-    TreeMap<String, Object> data = new TreeMap<String, Object>();
-    data.put("wow", "works?");
-    DeferredResult<ResponseEntity<?>> result = new DeferredResult<>();
-    meshService.getMesh().call("popcorn.poppy", data, new NucleoResponder(){
-      @Override
-      public void run(NucleoData data) {
-        result.setResult(ResponseEntity.ok(data));
-      }
-    });
-    return result;
-  }
-
-  @GetMapping("/timeout")
-  public DeferredResult<ResponseEntity<?>> getTimeout(){
-    TreeMap<String, Object> data = new TreeMap<String, Object>();
-    data.put("wow", "works?");
-    DeferredResult<ResponseEntity<?>> result = new DeferredResult<>();
-    meshService.getMesh().call(new String[]{"information.hits","information.x"}, data, new NucleoResponder(){
-      @Override
-      public void run(NucleoData data) {
-        result.setResult(ResponseEntity.ok(data));
-      }
-    });
-    return result;
-  }
-
-  @GetMapping("/")
-  public DeferredResult<ResponseEntity<?>> getRoot(){
-    TreeMap<String, Object> data = new TreeMap<String, Object>();
-    data.put("wow", "works?");
-    DeferredResult<ResponseEntity<?>> result = new DeferredResult<>();
-    meshService.getMesh().call("information.hits", data, new NucleoResponder(){
-      @Override
-      public void run(NucleoData data) {
-        result.setResult(ResponseEntity.ok(data));
-      }
-    });
-    return result;
-  }
-  @GetMapping("/c")
-  public DeferredResult<ResponseEntity<?>> getChange(){
-    TreeMap<String, Object> data = new TreeMap<String, Object>();
-    data.put("wow", "works?");
-    DeferredResult<ResponseEntity<?>> result = new DeferredResult<>();
-    meshService.getMesh().call(new String[]{"information.changeme","information.popcorn.two"}, data, new NucleoResponder(){
-      @Override
-      public void run(NucleoData data) {
-        result.setResult(ResponseEntity.ok(data));
-      }
-    });
-    return result;
-  }
-  @GetMapping("/chain")
-  public DeferredResult<ResponseEntity<?>> chainBreak(){
-    TreeMap<String, Object> data = new TreeMap<String, Object>();
-    data.put("stop", "chain");
-    DeferredResult<ResponseEntity<?>> result = new DeferredResult<>();
-    meshService.getMesh().call("information.changeme", data, new NucleoResponder(){
-      @Override
-      public void run(NucleoData data) {
-        result.setResult(ResponseEntity.ok(data));
-      }
-    });
-    return result;
-  }
-  @PostMapping("/req")
-  public DeferredResult<ResponseEntity<?>> request(@RequestParam("chains") String chains,
-                                                   @RequestParam("objects") String objects,
-                                                   HttpServletRequest request,
-                                                   HttpServletResponse response){
-    TreeMap<String, Object> data = (TreeMap<String, Object>) request.getAttribute("data");
-    try {
-      data.putAll(new ObjectMapper().readValue(objects, TreeMap.class));
-
-      chains = "session.get.continue,permission.admin,"+chains;
-      DeferredResult<ResponseEntity<?>> result = new DeferredResult<>();
-      meshService.getMesh().call(chains.split(","), data, new NucleoResponder() {
-        @Override
-        public void run(NucleoData data) {
-          result.setResult(ResponseEntity.ok(data));
-        }
-      });
-      return result;
-    }catch (Exception e){
-      e.printStackTrace();
+    @GetMapping("/prev")
+    public DeferredResult<ResponseEntity<?>> getPrev() {
+        TreeMap<String, Object> data = new TreeMap<String, Object>();
+        data.put("wow", "works?");
+        DeferredResult<ResponseEntity<?>> result = new DeferredResult<>();
+        meshService.getMesh().call("popcorn.poppy", data, new NucleoResponder() {
+            @Override
+            public void run(NucleoData data) {
+                result.setResult(ResponseEntity.ok(data));
+            }
+        });
+        return result;
     }
-    return null;
-  }
-  @ExceptionHandler({Exception.class})
-  public Object handleErrors() {
-    return new Object[]{"error", "form incomplete"};
-  }
+
+    @GetMapping("/timeout")
+    public DeferredResult<ResponseEntity<?>> getTimeout() {
+        TreeMap<String, Object> data = new TreeMap<String, Object>();
+        data.put("wow", "works?");
+        DeferredResult<ResponseEntity<?>> result = new DeferredResult<>();
+        meshService.getMesh().call(new String[]{"information.hits", "information.x"}, data, new NucleoResponder() {
+            @Override
+            public void run(NucleoData data) {
+                result.setResult(ResponseEntity.ok(data));
+            }
+        });
+        return result;
+    }
+
+    @GetMapping("/")
+    public DeferredResult<ResponseEntity<?>> getRoot() {
+        TreeMap<String, Object> data = new TreeMap<String, Object>();
+        data.put("wow", "works?");
+        DeferredResult<ResponseEntity<?>> result = new DeferredResult<>();
+        meshService.getMesh().call("information.hits", data, new NucleoResponder() {
+            @Override
+            public void run(NucleoData data) {
+                result.setResult(ResponseEntity.ok(data));
+            }
+        });
+        return result;
+    }
+
+    @GetMapping("/c")
+    public DeferredResult<ResponseEntity<?>> getChange() {
+        TreeMap<String, Object> data = new TreeMap<String, Object>();
+        data.put("wow", "works?");
+        DeferredResult<ResponseEntity<?>> result = new DeferredResult<>();
+        meshService.getMesh().call(
+            new String[]{
+                "information.hits",
+                "[information.changeme/information.popcorn/information.test]"
+            },
+            data,
+            new NucleoResponder() {
+                @Override
+                public void run(NucleoData data) {
+                    result.setResult(ResponseEntity.ok(data));
+                }
+            }
+        );
+        return result;
+    }
+
+    @GetMapping("/chain")
+    public DeferredResult<ResponseEntity<?>> chainBreak() {
+        TreeMap<String, Object> data = new TreeMap<String, Object>();
+        data.put("stop", "chain");
+        DeferredResult<ResponseEntity<?>> result = new DeferredResult<>();
+        meshService.getMesh().call("information.changeme", data, new NucleoResponder() {
+            @Override
+            public void run(NucleoData data) {
+                result.setResult(ResponseEntity.ok(data));
+            }
+        });
+        return result;
+    }
+
+    @PostMapping("/req")
+    public DeferredResult<ResponseEntity<?>> request(@RequestParam("chains") String chains,
+                                                     @RequestParam("objects") String objects,
+                                                     HttpServletRequest request,
+                                                     HttpServletResponse response) {
+        TreeMap<String, Object> data = (TreeMap<String, Object>) request.getAttribute("data");
+        try {
+            data.putAll(new ObjectMapper().readValue(objects, TreeMap.class));
+
+            chains = "session.get.continue,permission.admin," + chains;
+            DeferredResult<ResponseEntity<?>> result = new DeferredResult<>();
+            meshService.getMesh().call(chains.split(","), data, new NucleoResponder() {
+                @Override
+                public void run(NucleoData data) {
+                    result.setResult(ResponseEntity.ok(data));
+                }
+            });
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @ExceptionHandler({Exception.class})
+    public Object handleErrors() {
+        return new Object[]{"error", "form incomplete"};
+    }
 }
