d0a3c924a51e9524b18ca84ea47179682f081cbe
Nathaniel <nathaniel.davidson@gmail.com>
2019-12-02 00:46:46 -0800

    rest controller

--- src/main/java/com/nucleomesh/spring/springexample/rest/BanLookup.java
+++ src/main/java/com/nucleomesh/spring/springexample/rest/BanLookup.java
@@ -10,16 +10,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.TreeMap;
 
-@RestController("bans")
+@RestController
+@RequestMapping("bans")
 public class BanLookup {
 
     @Autowired
